31270b40a24cc3aedc21fd6fac45a9be94884388
olewandowski1 <olewandowski@soldevelo.com>
2025-04-26 00:21:13 +0200

    feat: new header, block details page adjustmnets

--- src/app/(app)/blocks/[category]/page.tsx
+++ src/app/(app)/blocks/[category]/page.tsx
@@ -1,8 +1,11 @@
 import { notFound } from 'next/navigation';
 
+import { CenteredHeader } from '@/registry/new-york/blocks/landing/header/centered-header/page';
+import { Separator } from '@/registry/new-york/ui/separator';
+
+import { MaxWidthWrapper } from '@/components/max-width-wrapper';
 import { getCategory } from '@/lib/config';
 import { getComponentsByNames } from '@/lib/utils';
-import { MaxWidthWrapper } from '@/components/max-width-wrapper';
 
 type CategoryPageProps = {
   params: Promise<{ category: string }>;
@@ -19,11 +22,26 @@ export default async function CategoryPage({ params }: CategoryPageProps) {
     currentCategory.components.map((comp) => comp.name)
   );
 
-  // TODO: Implement the category page layout
   return (
     <section>
       <MaxWidthWrapper>
-        <div className='animate-appear'> 123 </div>
+        <div className='space-y-4 animate-appear'>
+          <CenteredHeader
+            caption={`${currentCategory.name} Blocks`}
+            title={`${currentCategory.name} Examples`}
+            description={`Explore our collection of ${currentCategory.name} blocks, designed to help you build beautiful and functional UIs quickly and easily.`}
+          />
+
+          <Separator />
+
+          <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {categoryComponents.map((component) => {
+              console.log(component);
+
+              return <div key={component.name}> {component.name} </div>;
+            })}
+          </div>
+        </div>
       </MaxWidthWrapper>
     </section>
   );
---
+++ src/registry/new-york/blocks/landing/header/centered-header/page.tsx
@@ -0,0 +1,27 @@
+import { MaxWidthWrapper } from './components/max-width-wrapper';
+
+type CenteredHeaderProps = {
+  caption?: string;
+  title?: string;
+  description?: string;
+};
+
+export function CenteredHeader({
+  caption = 'Browse Blocks',
+  title = 'The collection of UI blocks',
+  description = 'Explore our collection of UI blocks, designed to help you build beautiful and functional user interfaces quickly and easily.',
+}: CenteredHeaderProps) {
+  return (
+    <MaxWidthWrapper>
+      <div className='max-w-xl mx-auto space-y-3 text-center animate-appear'>
+        <span className='text-sm font-medium text-primary'>{caption}</span>
+        <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>
+          {title}
+        </h1>
+        <p className='text-md text-muted-foreground sm:text-lg'>
+          {description}
+        </p>
+      </div>
+    </MaxWidthWrapper>
+  );
+}
