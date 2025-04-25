471d614785015c79ff024547ee096e4e2d916433
olewandowski1 <olewandowski@soldevelo.com>
2025-04-25 12:29:19 +0200

    feat: registry init

---
+++ src/app/icon.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from 'next/og';
+
+const Icon = () => {
+  let fill = 'olive';
+
+  if (process.env.NODE_ENV === 'development') {
+    fill = 'green';
+  }
+
+  return new ImageResponse(
+    (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='64'
+        height='64'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke={fill}
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      >
+        <path d='M22 2 2 22' />
+      </svg>
+    ),
+    {
+      width: 64,
+      height: 64,
+    }
+  );
+};
+
+export default Icon;
